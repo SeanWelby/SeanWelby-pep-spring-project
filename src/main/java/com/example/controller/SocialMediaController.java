027be3e91bdc7b35aff21461a7ee4f8456290000
SeanWelby <90876569+SeanWelby@users.noreply.github.com>
2025-04-08 11:07:55 -0400

    Update SocialMediaController.java

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -12,14 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- LOCAL CHANGE!!!
- sdfdsf
- */
+
 
  @RestController
 public class SocialMediaController {
